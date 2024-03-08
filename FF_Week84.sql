8c567286acfa4ff7db3d69f262500d8c9cb26b8a
Keith Evans <keith.evans@right-triangle.com>
2024-03-08 08:18:36 -0500

    First commit

---
+++ FF_Week84.sql
@@ -0,0 +1,14 @@
+CREATE STAGE frosty_aws_stage
+  URL = 's3://frostyfridaychallenges/';
+
+LIST @FROSTY_AWS_STAGE/challenge_84/ PATTERN='.*__.*[.]txt';
+
+CREATE STAGE WEEK84;
+
+
+COPY FILES
+  INTO @WEEK84
+  FROM @frosty_aws_stage
+  PATTERN='.*__.*[.]txt';
+
+  RM @WEEK84;
